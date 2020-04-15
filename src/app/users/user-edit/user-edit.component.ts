71ddee1a5527c6802601354138f260eb70f1a181
Alexandr Kulikovski <alikk@tut.by>
2020-04-15 14:14:36 +0300

    Form validation

--- src/app/users/user-edit/user-edit.component.ts
+++ src/app/users/user-edit/user-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
-import { FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 import { User, AccessType } from '../../shared/interfaces';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-edit',
@@ -20,21 +21,59 @@ export class UserEditComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor() { }
+  minNameLength = 5;
+  minUserNameLength = 2;
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+
     this.form = new FormGroup({
-      id: new FormControl(''),
-      name: new FormControl(''),
-      username: new FormControl(''),
-      email: new FormControl(''),
-      phone: new FormControl(''),
+      id: new FormControl({value: '', disabled: true}),
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(this.minNameLength)
+      ]),
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(this.minUserNameLength)
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      phone: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[0-9]*')
+      ]),
       website: new FormControl(''),
-      accessType: new FormControl('')
+      accessType: new FormControl('visitor')
     });
-    if (this.currentUserId !== '0') {
+
+    if (!this.currentUserId) {
+      this.router.navigate(['user-list']);
+    } else if (this.currentUserId !== '0') {
       this.form.patchValue(this.currentUser);
     }
+
+  }
+
+  get name() {
+    return this.form.get('name');
+  }
+
+  get username() {
+    return this.form.get('username');
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get phone() {
+    return this.form.get('phone');
   }
 
   submit() {
