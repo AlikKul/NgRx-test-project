04aab78ef9e3ebbfac4609a9dbd2d18b46fb9088
Alexandr Kulikovski <alikk@tut.by>
2020-05-14 15:51:52 +0300

    Tabs in dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,12 +4,14 @@ import { SharedModule } from './../shared/shared.module';
 import { DashboardHomeComponent } from './dashboard-home/dashboard-home.component';
 import { DashboardHomeContainerComponent } from './dashboard-home/dashboard-home-container.component';
 import { HighchartsChartModule } from 'highcharts-angular';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     SharedModule,
     CommonModule,
-    HighchartsChartModule
+    HighchartsChartModule,
+    NgbModule
   ],
   declarations: [
     DashboardHomeComponent,
